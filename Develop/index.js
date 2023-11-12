673353e5db185daf7ac8cbc2f07bed0f75828e02
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-11-12 15:44:24 -0500

    installed inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inq = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     inq
+//         .prompt(questions)
+//         .then
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
+
+// inq.prompt([
+//     {
+//         type: 'input',
+//         message: 'What is your name?',
+//         name: 'username'
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your age?',
+//         name: 'age'
+//     }
+// ])
+//     .then((response) =>
+//         console.log(response.age)
+//     );
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+# Tools Used
+https://www.npmjs.com/package/inquirer/v/8.2.4
+
+# Credits
+https://www.educative.io/answers/how-to-use-the-inquirer-node-package
