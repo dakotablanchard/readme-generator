bc42aa68d1f396fef7d77896dcb78efa27c9f12a
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-11-12 17:09:34 -0500

    Got quesetions to work, and data is able to be accessed in writeToFile function

--- Develop/index.js
+++ Develop/index.js
@@ -3,21 +3,54 @@ const fs = require('fs');
 const inq = require("inquirer");
 
 // TODO: Create an array of questions for user input
-// const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Give a short description of the app:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What is the installation process of your app?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'How do you use the app?',
+        name: 'usage'
+    }
+];
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     inq
-//         .prompt(questions)
-//         .then
-// }
+function writeToFile(data) {
+    fileName = data.title
+    console.log(fileName)
+    // fs.appendFile(
+    //     `${fileName}.md`,
+    //     `${process.argv[2]}\n`,
+    //     (err) => err ? console.error(err) : console.log('Commit logged!'))
+}
 
 // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+    inq
+        .prompt(questions)
+        .then((data) => {
+            writeToFile(data)
+        });
+}
 
 // Function call to initialize app
-// init();
+init();
 
+
+
+// PROMPT EXAMPLE FOR REFERENCE
 // inq.prompt([
 //     {
 //         type: 'input',
